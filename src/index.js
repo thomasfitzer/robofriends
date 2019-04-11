04d71ed25d7fc8091a059085e8d5591ece1a0f62
hockey6409 <thomasfitzer89@gmail.com>
2019-04-11 18:40:39 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import Card from './Card';
+import App from './App'
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
